dd1c853a96b4395dd30b387b064eff9922c73671
casse <devnull@localhost>
2009-11-13 12:06:10 +0000

    Improved examples with new forms.

--- test/asm.c
+++ test/asm.c
@@ -2,5 +2,6 @@
  static inline int cc_sl_trylock(int *lock) {
 	int r = 1;
 	__asm("swap.w %0,[%2]0" : "=d" (r) : "0" (r), "a" ((int *) lock) : "memory");
+	__asm("bl ok" : : );
 	return r;
 }
--- test/melted.c
+++ test/melted.c
@@ -3,6 +3,9 @@
 int f(void) {
 	int x;
 	x = 0;
+	x++;
 	int y;
 	y = 0;
+	z = 0;
+	int u;
 }
